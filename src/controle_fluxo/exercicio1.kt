d9191dfb78167df8428a7b90d2c26fbc84d35082
LucOliverfn <lucas.ofn@gmail.com>
2024-02-01 15:48:39 -0300

    estruturas de controle de fluxo

---
+++ src/controle_fluxo/exercicio1.kt
@@ -0,0 +1,51 @@
+package controle_fluxo
+
+fun ex1(cargo:String):Float{
+    var bonus = 0f
+    if (cargo=="Gerente"){
+        bonus =2000f
+    }
+    else if (cargo=="Coordenador"){
+        bonus= 1500f
+    }
+    else if (cargo=="Engenheiro"){
+        bonus= 1000f
+    }
+    else if (cargo=="Estagiario"){
+        bonus= 500f
+    }
+    return (bonus)
+}
+fun ex2(cargo:String, ano:Int):Float{
+    var bonus = 0f
+    if (cargo=="Gerente"){
+        if (ano<=2){
+            bonus = 2000f
+        }else{
+            bonus =3000f
+        }
+    }
+    else if (cargo=="Coordenador"){
+        if (ano<=1){
+            bonus = 1500f
+        }else{
+            bonus =1800f
+        }
+    }
+    else if (cargo=="Engenheiro"){
+        bonus=1000f
+    }
+    else if (cargo=="Estagiario"){
+        bonus=500f
+    }
+    return (bonus)
+}
+
+fun main(){
+    val bonus = "Gerente"
+    println("O bonus para o cargo $bonus é de ${ex1(bonus)}")
+    println("O bonus para o cargo $bonus é de ${ex2(bonus,3)}")
+}
+
+
+
---
+++ src/controle_fluxo/if_ex2.kt
@@ -0,0 +1,21 @@
+package controle_fluxo
+
+fun main(){
+    val id = 40
+    maiorDeIdade(id)
+    saudacoes(true)
+}
+fun maiorDeIdade(idade:Int){
+    if (idade>=18){
+         println("Ele é maior de idade")
+    } else{
+        println("Ele é menor de idade")
+    }
+}
+fun saudacoes (dia:Boolean):String{
+    return if (dia){
+        "BOM DIA"
+    } else{
+        return "boa noite"
+    }
+}
